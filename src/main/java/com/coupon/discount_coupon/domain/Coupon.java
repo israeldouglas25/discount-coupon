fa9e61a6bf12f9c68e366aa5bc7e6d468c5c9e6b
israel.paixao <israeldouglas25@gmail.com>
2025-12-04 11:32:52 -0300

    Ajustando a classe para persistir no BD

--- src/main/java/com/coupon/discount_coupon/domain/Coupon.java
+++ src/main/java/com/coupon/discount_coupon/domain/Coupon.java
@@ -1,29 +1,29 @@
 package com.coupon.discount_coupon.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
-//@Entity
-//@Table(name = "coupons")
+@Entity
+@Table(name = "tb_coupons")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-//@EqualsAndHashCode(of = "id")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Coupon {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @EqualsAndHashCode.Include
+    private UUID id;
     private String code;
     private String description;
     private BigDecimal discountValue;
     private Instant expirationDate;
-
+    private Boolean published;
 
 }
