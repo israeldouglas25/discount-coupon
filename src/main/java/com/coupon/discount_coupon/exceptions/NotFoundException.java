a669b71631b4a4c8395f873cdbbf4f4e4a0095ef
israel.paixao <israeldouglas25@gmail.com>
2025-12-05 11:03:48 -0300

    tratamento de erro

---
+++ src/main/java/com/coupon/discount_coupon/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.coupon.discount_coupon.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
